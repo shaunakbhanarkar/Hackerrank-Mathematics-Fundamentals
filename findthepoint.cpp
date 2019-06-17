4566f1cb2ffbc798f13ea645b1124f4485f0f73c
Shaunak Bhanarkar <shaunak.bhanarkar@gmail.com>
2019-06-17 20:23:49 +0530

    Create findthepoint.cpp

---
+++ findthepoint.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int px,py,qx,qy;
+        cin>>px>>py>>qx>>qy;
+        cout<<-px+2*qx<<" ";
+        cout<<-py+2*qy<<" "<<endl;
+
+    }
+}
